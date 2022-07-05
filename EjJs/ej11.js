22594a6b7cda6b59395601d09637753355e57f84
Mariacanas21 <75243815+Mariacanas21@users.noreply.github.com>
2022-07-05 10:56:06 +0200

    Update ej11.js

--- EjJs/ej11.js
+++ EjJs/ej11.js
@@ -2,14 +2,15 @@
 
 
 
-    let splitArrayIntoChunks = (arr,tam) =>{ 
-        var newArray = [];
+   const splitArrayIntoChunks = (arr,tam) => { 
+        let newArray = [];
         for(let i=0; i < arr.length; i+=tam){
-            newArray.push((arr.slice(i,i+tam)) + '<br>'); 
+            newArray.push((arr.slice(i,i+tam))); 
         }
         return newArray;
     }
 
     const result = splitArrayIntoChunks([1,2,3,4,5,6,7],3);
-    document.write(result);
+    console.log(result);
+    
 
